ff01399495cbd889213f4b28d41b6137b5c3a675
Liu Lixiang <liulixiang1988@gmail.com>
2015-06-11 16:57:56 +0800

    add static files

--- app_host/settings.py
+++ app_host/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.8/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
-
+DEBUG=True
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
@@ -103,3 +103,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_only')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static', 'static'),
+)
--- app_host/urls.py
+++ app_host/urls.py
@@ -21,5 +21,5 @@ from app import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^app/', include('app.urls')),
+    url(r'^app/', include('app.urls', namespace="app")),
 ]
